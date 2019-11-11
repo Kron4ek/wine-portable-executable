70a7b05bb92f8fac905cc1a909d3c5dada0d1463
Kron <kron4ek@gmail.com>
2019-11-11 14:45:45 +0500

    Update create_wine_portable.sh

--- create_wine_portable.sh
+++ create_wine_portable.sh
@@ -2,13 +2,21 @@
 
 ## Required packages: squashfs-tools wget
 
-export working_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"/wine-portable
+script_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+working_dir="${script_dir}"/wine-portable
 
+# You can change urls below if you want to use different Wine build,
+# different runtime or scripts
 wine_url="https://github.com/Kron4ek/Wine-Builds/releases/download/4.19/wine-4.19-staging-amd64.tar.xz"
-wine_runtime_url="https://github.com/Kron4ek/wine-portable-executable/raw/master/wine-runtime.tar.xz"
+wine_runtime_url="https://github.com/Kron4ek/wine-portable-executable/raw/master/binaries/wine-runtime.tar.xz"
 wine_start_script_url="https://raw.githubusercontent.com/Kron4ek/wine-portable-executable/master/wine.sh"
 squashfs_start_script_url="https://raw.githubusercontent.com/Kron4ek/wine-portable-executable/master/squashfs-start.sh"
-squashfuse_archive_url="https://github.com/Kron4ek/wine-portable-executable/raw/master/squashfuse.tar"
+squashfuse_archive_url="https://github.com/Kron4ek/wine-portable-executable/raw/master/binaries/squashfuse.tar"
+
+# Available compressors: gzip, lzma, lzo, lz4, xz, zstd
+# See the mksquashfs documentation for more information
+squashfs_compressor=lz4
+compressor_arguments="-Xhc"
 
 mkdir -p "${working_dir}"/squashfs-root
 
@@ -22,9 +30,13 @@ if  [ ! -d wine ]; then
 fi
 
 if [ ! -d wine-runtime ]; then
-	wget -O wine-runtime.tar.xz "${wine_runtime_url}" -q --show-progress
-	tar xf wine-runtime.tar.xz
-	rm wine-runtime.tar.xz
+	if [ ! -f "${script_dir}"/binaries/wine-runtime.tar.xz ];
+		wget -O wine-runtime.tar.xz "${wine_runtime_url}" -q --show-progress
+		tar xf wine-runtime.tar.xz
+		rm wine-runtime.tar.xz
+	else
+		tar xf "${script_dir}"/binaries/wine-runtime.tar.xz
+	fi
 fi
 
 if [ ! -f wine.sh ]; then
@@ -35,7 +47,11 @@ fi
 cd "${working_dir}"
 
 if [ ! -f squashfuse.tar ]; then
-	wget -O squashfuse.tar "${squashfuse_archive_url}" -q --show-progress
+	if [ ! -f "${script_dir}"/binaries/squashfuse.tar ]; then
+		wget -O squashfuse.tar "${squashfuse_archive_url}" -q --show-progress
+	else
+		ln -sr "${script_dir}"/binaries/squashfuse.tar .
+	fi
 fi
 
 if [ ! -f squashfs-start.sh ]; then
@@ -43,9 +59,12 @@ if [ ! -f squashfs-start.sh ]; then
 	chmod +x squashfs-start.sh
 fi
 
+echo
 echo "Creating portable Wine executable..."
-mksquashfs squashfs-root wine.squashfs -comp lz4 -Xhc
+# Create squashfs image
+mksquashfs squashfs-root wine.squashfs -comp $squashfs_compressor $compressor_arguments
 
+# Combine files into a single executable using cat
 cat squashfs-start.sh squashfuse.tar wine.squashfs > wine-portable.sh
 chmod +x wine-portable.sh
 
