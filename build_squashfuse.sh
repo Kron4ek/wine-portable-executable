836e019a14195ecf80f1aeb2049e26f4d8631989
Kron4ek <Kron4ek@gmail.com>
2019-12-23 19:58:42 +0500

    Add scripts for creating runtime and squashfuse

---
+++ build_squashfuse.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+
+## This script creates squashfuse archive by using Ubuntu chroot
+
+scriptdir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+
+CHROOT_X64="/home/builder/chroots/bionic64_chroot"
+
+if [ ! -d "${CHROOT_X64}" ]; then
+	echo "Chroot is required for squashfuse compilation!"
+	exit 1
+fi
+
+if [ "$EUID" != 0 ]; then
+	echo "Root rights are required for squashfuse compilation!"
+	exit 1
+fi
+
+build_in_chroot () {
+	CHROOT_PATH="$CHROOT_X64"
+
+	echo "Unmounting chroot directories"
+	umount -Rl "$CHROOT_PATH"
+
+	echo "Mount directories for chroot"
+	mount --bind "$CHROOT_PATH" "$CHROOT_PATH"
+	mount --bind /dev "$CHROOT_PATH/dev"
+	mount --bind /dev/shm "$CHROOT_PATH/dev/shm"
+	mount --bind /dev/pts "$CHROOT_PATH/dev/pts"
+	mount --bind /proc "$CHROOT_PATH/proc"
+	mount --bind /sys "$CHROOT_PATH/sys"
+
+	echo "Chrooting into $CHROOT_PATH"
+	chroot "$CHROOT_PATH" /usr/bin/env LANG=en_US.UTF-8 TERM=xterm PATH="/bin:/sbin:/usr/bin:/usr/sbin" /opt/build.sh
+
+	echo "Unmounting chroot directories"
+	umount -Rl "$CHROOT_PATH"
+}
+
+create_build_scripts () {
+	echo '#!/bin/bash' > $MAINDIR/build.sh
+	echo 'apt-get update' >> $MAINDIR/build.sh
+	echo 'apt-get -y install libfuse-dev' >> $MAINDIR/build.sh
+	echo 'mkdir /opt/build_squashfuse' >> $MAINDIR/build.sh
+	echo 'cd /opt/build_squashfuse' >> $MAINDIR/build.sh
+	echo 'wget -O lz4.tar.gz https://github.com/lz4/lz4/archive/v1.9.2.tar.gz' >> $MAINDIR/build.sh
+	echo 'wget -O zstd.tar.gz https://github.com/facebook/zstd/archive/v1.4.4.tar.gz' >> $MAINDIR/build.sh
+	echo 'wget -O squashfuse.tar.gz https://github.com/vasi/squashfuse/archive/0.1.103.tar.gz' >> $MAINDIR/build.sh
+	echo 'tar xf lz4.tar.gz' >> $MAINDIR/build.sh
+	echo 'tar xf zstd.tar.gz' >> $MAINDIR/build.sh
+	echo 'tar xf squashfuse.tar.gz' >> $MAINDIR/build.sh
+	echo 'cd lz4-1.9.2 && make -j$(nproc) && make install' >> $MAINDIR/build.sh
+	echo 'cd ../zstd-1.4.4 && make -j$(nproc) && make install' >> $MAINDIR/build.sh
+	echo 'cd ../squashfuse-0.1.103 && ./autogen.sh' >> $MAINDIR/build.sh
+	echo 'mkdir ../build && cd ../build' >> $MAINDIR/build.sh
+	echo '../squashfuse-0.1.103/configure --without-zlib --without-xz --without-lzo' >> $MAINDIR/build.sh
+	echo 'make -j$(nproc) && make install' >> $MAINDIR/build.sh
+	echo 'cd /opt && rm -r /opt/build_squashfuse' >> $MAINDIR/build.sh
+
+	chmod +x "$MAINDIR/build.sh"
+	mv "$MAINDIR/build.sh" "$CHROOT_X64/opt"
+}
+
+create_build_scripts
+build_in_chroot
+
+rm "$CHROOT_X64/opt/build.sh"
+
+rm -rf "${scriptdir}"/squashfuse
+mkdir -p "${scriptdir}"/squashfuse/squashfuse-amd64
+
+cp "${CHROOT_X64}"/usr/local/lib/libsquashfuse.so.0 "${scriptdir}"/squashfuse/squashfuse-amd64
+cp "${CHROOT_X64}"/usr/local/lib/libfuseprivate.so.0 "${scriptdir}"/squashfuse/squashfuse-amd64
+cp "${CHROOT_X64}"/usr/local/lib/liblz4.so.1 "${scriptdir}"/squashfuse/squashfuse-amd64
+cp "${CHROOT_X64}"/usr/local/lib/libzstd.so.1 "${scriptdir}"/squashfuse/squashfuse-amd64
+cp "${CHROOT_X64}"/usr/local/bin/squashfuse "${scriptdir}"/squashfuse/squashfuse-amd64
+
+cd "${scriptdir}" || exit 1
+
+tar -cf squashfuse.tar squashfuse
+
+rm -r squashfuse
+
+if [ -d binaries ]; then ln -sfr squashfuse.tar binaries; fi
+
+clear; echo "Done"
---
+++ create_wine_runtime.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+## This script creates runtime for Wine by using two Ubuntu chroots
+
+scriptdir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+
+CHROOT_X64="${HOME}/chroots/bionic64_chroot"
+CHROOT_X32="${HOME}/chroots/bionic32_chroot"
+
+if [ ! -d "${CHROOT_X64}" ] || [ ! -d "${CHROOT_X32}" ]; then
+	echo "Chroots are required for the runtime creation!"
+	exit 1
+fi
+
+if [ ! -f "${scriptdir}"/runtime_libs_list.txt ]; then
+	echo "runtime_libs_list.txt is required!"
+	exit 1
+fi
+
+rm -rf "${scriptdir}"/wine-runtime
+mkdir -p "${scriptdir}"/wine-runtime/lib32
+mkdir -p "${scriptdir}"/wine-runtime/lib
+
+echo "Copying libraries..."
+while read lib; do
+	if [ -f "${CHROOT_X32}"/usr/local/lib/"${lib}" ]; then
+		cp "${CHROOT_X32}"/usr/local/lib/"${lib}" "${scriptdir}"/wine-runtime/lib32 2>/dev/null
+	else
+		cp "${CHROOT_X32}"/lib/i386-linux-gnu/"${lib}" "${scriptdir}"/wine-runtime/lib32 2>/dev/null
+		cp "${CHROOT_X32}"/usr/lib/i386-linux-gnu/"${lib}" "${scriptdir}"/wine-runtime/lib32 2>/dev/null
+	fi
+
+	if [ -f "${CHROOT_X64}"/usr/local/lib/"${lib}" ]; then
+		cp "${CHROOT_X64}"/usr/local/lib/"${lib}" "${scriptdir}"/wine-runtime/lib 2>/dev/null
+	else
+		cp "${CHROOT_X64}"/lib/x86_64-linux-gnu/"${lib}" "${scriptdir}"/wine-runtime/lib 2>/dev/null
+		cp "${CHROOT_X64}"/usr/lib/x86_64-linux-gnu/"${lib}" "${scriptdir}"/wine-runtime/lib 2>/dev/null
+	fi
+done < "${scriptdir}"/runtime_libs_list.txt
+
+cd "${scriptdir}" || exit 1
+
+echo "Creating and compressing archive..."
+tar -cf wine-runtime.tar wine-runtime
+xz -9 wine-runtime.tar
+
+rm -r wine-runtime
+
+if [ -d binaries ]; then ln -sfr wine-runtime.tar.xz binaries; fi
+
+echo "Done"
