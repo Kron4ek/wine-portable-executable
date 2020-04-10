85ea25bc5bc29c4d29242644127f0cbf3bc3c69f
Kron4ek <kron4ek@gmail.com>
2020-04-10 15:40:14 +0500

    Add ability to use embedded winetricks

--- wine.sh
+++ wine.sh
@@ -6,5 +6,19 @@ if [ -z "$DISABLE_RUNTIME" ]; then
 	export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${script_dir}/wine-runtime/lib:${script_dir}/wine-runtime/lib32"
 fi
 
-"${script_dir}"/wine/bin/wine "$@"
-"${script_dir}"/wine/bin/wineserver -w
+export WINE="${script_dir}"/wine/bin/wine
+export WINE64="${script_dir}"/wine/bin/wine64
+export WINESERVER="${script_dir}"/wine/bin/wineserver
+export WINETRICKS="${script_dir}"/winetricks
+
+if [ "$1" = "winetricks" ]; then
+	if [ $# -ge 2 ]; then
+		shift
+		"${WINETRICKS}" "$@"
+	else
+		"${WINETRICKS}" --help
+	fi
+else
+	"${WINE}" "$@"
+	"${WINESERVER}" -w
+fi
